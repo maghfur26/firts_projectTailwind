15c48a96d478e65dc21ee07e771d92196695870b
maghfur_hasani <maghfurhasani9@gmail.com>
2023-12-24 20:45:54 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// humberger menu
+const humberger = document.querySelector('#humberger');
+const navMenu = document.querySelector('#nav-menu');
+
+humberger.addEventListener('click', function(){
+    humberger.classList.toggle('humberger-active')
+    navMenu.classList.toggle('hidden')
+})
+
+//navbar fixed
+window.onscroll = function(){
+    const header = document.querySelector('header')
+    const navfixed = header.offsetTop
+
+    if(window.scrollY > navfixed){
+        header.classList.add('navbar-fixed')
+    }else{
+        header.classList.remove('navbar-fixed')
+    }
+}
+
+//darkMode
+const togle = document.querySelector('#togle');
+const html = document.querySelector('html');
+
+togle.addEventListener('click', function() {
+    togle.checked ? html.classList.add('dark') : html.classList.remove('dark');
+})
